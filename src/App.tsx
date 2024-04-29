2288e19966c6a74f3d15f9445081f1b4bb0bc356
Vinicius Gouvea <vrgouvea@outlook.com>
2024-04-29 14:17:55 -0300

    login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import { RouterProvider } from 'react-router-dom'
 // import { Button } from './components/ui/button'
 import './global.css'
+import { HelmetProvider, Helmet } from 'react-helmet-async'
 import { router } from './routes'
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <HelmetProvider>
+      <Helmet titleTemplate="%s | pizza.shop" />
+      <RouterProvider router={router} />
+    </HelmetProvider>
+  )
 }

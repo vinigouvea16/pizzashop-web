c5bc380ddbd8fcaf678c7e1c7c401788d53a0ead
Vinicius Gouvea <vrgouvea@outlook.com>
2024-05-03 18:24:07 -0300

    utilizing spies

---
+++ src/components/pagination.spec.tsx
@@ -0,0 +1,42 @@
+import { render } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
+import { Pagination } from './pagination'
+
+const onPageChangeCallBack = vi.fn()
+
+describe('Pagination', () => {
+  it('should display the right amount of pages and results', () => {
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        onPageChange={() => {}}
+      />,
+    )
+
+    expect(wrapper.getByText('Página 1 de 20')).toBeInTheDocument()
+    expect(wrapper.getByText('Total de 200 item(s)')).toBeInTheDocument()
+  })
+
+  it('should be able to navigate to the next page', async () => {
+    const user = userEvent.setup()
+
+    const wrapper = render(
+      <Pagination
+        pageIndex={0}
+        totalCount={200}
+        perPage={10}
+        onPageChange={onPageChangeCallBack}
+      />,
+    )
+
+    const nextPageButton = wrapper.getByRole('button', {
+      name: 'Próxima página',
+    })
+
+    await user.click(nextPageButton)
+
+    expect(onPageChangeCallBack).toHaveBeenCalled(1)
+  })
+})

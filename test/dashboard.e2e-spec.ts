3fa8cc3a0cb4aa4b240e4534416c32335a809eae
Vinicius Gouvea <vrgouvea@outlook.com>
2024-05-04 23:36:30 -0300

    async assertions and closure

--- test/dashboard.e2e-spec.ts
+++ test/dashboard.e2e-spec.ts
@@ -3,27 +3,27 @@ import { test, expect } from '@playwright/test'
 test('display day orders amount metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
 
-  expect(page.getByText('20', { exact: true }).nth(1)).toBeVisible()
-  expect(page.getByText('-5% em relação à ontem')).toBeVisible()
+  await expect(page.getByText('20', { exact: true }).nth(1)).toBeVisible()
+  await expect(page.getByText('-5% em relação à ontem')).toBeVisible()
 })
 
 test('display month orders amount metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
 
-  expect(page.getByText('20', { exact: true }).first()).toBeVisible()
-  expect(page.getByText('+7% em relação ao mês passado')).toBeVisible()
+  await expect(page.getByText('20', { exact: true }).first()).toBeVisible()
+  await expect(page.getByText('+7% em relação ao mês passado')).toBeVisible()
 })
 
 test('display month canceled orders amount metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
 
-  expect(page.getByText('5', { exact: true })).toBeVisible()
-  expect(page.getByText('-5% em relação à ontem')).toBeVisible()
+  await expect(page.getByText('5', { exact: true })).toBeVisible()
+  await expect(page.getByText('-5% em relação à ontem')).toBeVisible()
 })
 
 test('display month revenue metric', async ({ page }) => {
   await page.goto('/', { waitUntil: 'networkidle' })
 
-  expect(page.getByText('R$ 200,00')).toBeVisible()
-  expect(page.getByText('+10% em relação ao mês passado')).toBeVisible()
+  await expect(page.getByText('R$ 200,00')).toBeVisible()
+  await expect(page.getByText('+10% em relação ao mês passado')).toBeVisible()
 })
--- test/orders.e2e-spec.ts
+++ test/orders.e2e-spec.ts
@@ -3,10 +3,10 @@ import { test, expect } from '@playwright/test'
 test('list orders', async ({ page }) => {
   await page.goto('/orders', { waitUntil: 'networkidle' })
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 1', exact: true }),
   ).toBeVisible()
-  expect(page.getByRole('cell', { name: 'Customer 10' })).toBeVisible()
+  await expect(page.getByRole('cell', { name: 'Customer 10' })).toBeVisible()
 })
 
 test('paginate orders', async ({ page }) => {
@@ -14,31 +14,31 @@ test('paginate orders', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Próxima página' }).click()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 11', exact: true }),
   ).toBeVisible()
-  expect(page.getByRole('cell', { name: 'Customer 20' })).toBeVisible()
+  await expect(page.getByRole('cell', { name: 'Customer 20' })).toBeVisible()
 
   await page.getByRole('button', { name: 'Última página' }).click()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 51', exact: true }),
   ).toBeVisible()
-  expect(page.getByRole('cell', { name: 'Customer 60' })).toBeVisible()
+  await expect(page.getByRole('cell', { name: 'Customer 60' })).toBeVisible()
 
   await page.getByRole('button', { name: 'Página Anterior' }).click()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 41', exact: true }),
   ).toBeVisible()
-  expect(page.getByRole('cell', { name: 'Customer 50' })).toBeVisible()
+  await expect(page.getByRole('cell', { name: 'Customer 50' })).toBeVisible()
 
   await page.getByRole('button', { name: 'Primeira página' }).click()
 
-  expect(
+  await expect(
     page.getByRole('cell', { name: 'Customer 1', exact: true }),
   ).toBeVisible()
-  expect(page.getByRole('cell', { name: 'Customer 10' })).toBeVisible()
+  await expect(page.getByRole('cell', { name: 'Customer 10' })).toBeVisible()
 })
 
 test('filter by order id', async ({ page }) => {
@@ -48,7 +48,7 @@ test('filter by order id', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Filtrar resultados' }).click()
 
-  expect(page.getByRole('cell', { name: 'order-11' })).toBeVisible()
+  await expect(page.getByRole('cell', { name: 'order-11' })).toBeVisible()
 })
 
 test('filter by customer name', async ({ page }) => {
@@ -58,7 +58,7 @@ test('filter by customer name', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Filtrar resultados' }).click()
 
-  expect(page.getByRole('cell', { name: 'Customer 11' })).toBeVisible()
+  await expect(page.getByRole('cell', { name: 'Customer 11' })).toBeVisible()
 })
 
 test('filter by status', async ({ page }) => {
@@ -69,9 +69,5 @@ test('filter by status', async ({ page }) => {
 
   await page.getByRole('button', { name: 'Filtrar resultados' }).click()
 
-  const tableRows = await page.getByRole('cell', { name: 'Pendente' }).all()
-
-  expect(tableRows).toHaveLength(10)
-
-  await page.waitForTimeout(250)
+  await expect(page.getByRole('cell', { name: 'Pendente' })).toHaveCount(10)
 })
